73b0f8b7300f92c780019fbd46b96e829dd999f2
Barden <119883267+Barden-dev@users.noreply.github.com>
2025-06-22 12:24:06 +0300

    Update restartbot.sh

--- scripts/restartbot.sh
+++ scripts/restartbot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-systemctl restart tgbot.service
+systemctl restart tgbot.service #if you want restart, you can change this file for your purposes, in my implementation, i user systemctl service
