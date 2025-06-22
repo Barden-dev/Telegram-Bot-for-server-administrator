7f353299e588e50a812300fb0e774fe1083ac2f9
Barden <119883267+Barden-dev@users.noreply.github.com>
2025-06-22 12:25:52 +0300

    Update config.py

--- python/config.py
+++ python/config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "[bot:token]" put here Bot Token from BotFather without []
-homeFolder = "[home/folder]" put here folder to project without []
+BOT_TOKEN = "[bot:token]" #put here Bot Token from BotFather without []
+homeFolder = "[home/folder]" #put here folder to project without []
 ADMINS = [1234567890] #put here your TG id
